9747f53bd830193884facd2f9eb88f05b380d89a
duporov <ygmypt18@yandex.ru>
2020-03-08 14:08:47 +0300

    moved selector animation to itself

--- lib/DatePicker.dart
+++ lib/DatePicker.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp2020/DatePickerLine.dart';
-import 'package:intl/intl.dart';
 
 import 'DatePickerSelector.dart';
 
 class DatePicker extends StatefulWidget {
-  const DatePicker(double width) :_width = width;
+  const DatePicker(double width) : _width = width;
 
   final double _width;
 
@@ -21,10 +20,7 @@ class _DatePickerState extends State<DatePicker> {
   final double _selectorHeight = 96.0;
   final double _selectorCircleIncreaseCoefficient = 1.8;
 
-  double _firstSelectorPosition = 100.0;
-  double _secondSelectorPosition = 260.0;
-
-  DateTime _currentDate = DateTime.now();
+  final DateTime _currentDate = DateTime.now();
   DateTime _firstSelectorDate;
   DateTime _secondSelectorDate;
 
@@ -36,36 +32,6 @@ class _DatePickerState extends State<DatePicker> {
     _datesWithPositions = _initDates;
     _firstSelectorDate = _currentDate.subtract(Duration(days: 30));
     _secondSelectorDate = _currentDate;
-    _updateFirstSelectorPosition();
-    _updateSecondSelectorPosition();
-  }
-
-  ///
-  /// `force` mean ignore current selector date
-  ///
-  void _updateFirstSelectorPosition({bool force = false}) {
-    if (_firstSelectorDate == null || force) {
-      _firstSelectorDate = _findCloserDateByPosition(_firstSelectorPosition);
-    }
-    _firstSelectorPosition = _positionByDate(
-      _firstSelectorDate,
-      _firstSelectorPosition,
-      _width,
-    );
-  }
-
-  ///
-  /// `force` mean ignore current selector date
-  ///
-  void _updateSecondSelectorPosition({bool force = false}) {
-    if (_secondSelectorDate == null || force) {
-      _secondSelectorDate = _findCloserDateByPosition(_secondSelectorPosition);
-    }
-    _secondSelectorPosition = _positionByDate(
-      _secondSelectorDate,
-      _secondSelectorPosition,
-      _width,
-    );
   }
 
   @override
@@ -84,92 +50,29 @@ class _DatePickerState extends State<DatePicker> {
             callback: (datesWithPositions) {
               setState(() {
                 _datesWithPositions = datesWithPositions;
-                _updateFirstSelectorPosition();
-                _updateSecondSelectorPosition();
               });
             },
           ),
         ),
-        AnimatedPositioned(
-          duration: Duration(milliseconds: 50),
-          left: _firstSelectorPosition,
-          child: DatePickerSelector(
-            width: _selectorWidth,
-            height: _selectorHeight,
-            circleIncreaseCoefficient: _selectorCircleIncreaseCoefficient,
-            onPanUpdate: (details) {
-              setState(() {
-                _firstSelectorPosition =
-                    details.globalPosition.dx - (_selectorWidth / 2);
-                _updateFirstSelectorPosition(force: true);
-              });
-            },
-          ),
+        DatePickerSelector(
+          _datesWithPositions,
+          width: _selectorWidth,
+          height: _selectorHeight,
+          lineWidth: _width,
+          circleIncreaseCoefficient: _selectorCircleIncreaseCoefficient,
+          initDate: _firstSelectorDate,
+          callback: (date) => _firstSelectorDate = date,
         ),
-        AnimatedPositioned(
-          duration: Duration(milliseconds: 50),
-          left: _secondSelectorPosition,
-          child: Stack(
-            children: <Widget>[
-              DatePickerSelector(
-                width: _selectorWidth,
-                height: _selectorHeight,
-                circleIncreaseCoefficient: _selectorCircleIncreaseCoefficient,
-                onPanUpdate: (details) {
-                  setState(() {
-                    _secondSelectorPosition =
-                        details.globalPosition.dx - (_selectorWidth / 2);
-                    _updateSecondSelectorPosition(force: true);
-                  });
-                },
-              ),
-              Text(
-                _secondSelectorDate == null
-                    ? "null"
-                    : DateFormat.d().format(_secondSelectorDate),
-                style: TextStyle(fontSize: 20.0, color: Colors.white),
-              ),
-            ],
-          ),
+        DatePickerSelector(
+          _datesWithPositions,
+          width: _selectorWidth,
+          height: _selectorHeight,
+          lineWidth: _width,
+          circleIncreaseCoefficient: _selectorCircleIncreaseCoefficient,
+          initDate: _secondSelectorDate,
+          callback: (date) => _secondSelectorDate = date,
         ),
       ],
     );
-  }
-
-  DateTime _findCloserDateByPosition(double position) {
-    final date = _findCloserDateWithPositionByPosition(position);
-    return date == null ? null : date.date;
-  }
-
-  DateWithPosition _findCloserDateWithPositionByPosition(double position) {
-    if (_datesWithPositions != null) {
-      for (DateWithPosition date in _datesWithPositions) {
-        final delta = (date.position - position).abs();
-        if (delta <= 5.0) {
-          return date;
-        }
-      }
-    }
-    return null;
-  }
-
-  double _positionByDate(DateTime date,
-      double fallbackPosition,
-      double maxPosition,) {
-    if (date == null) return fallbackPosition;
-    if (_datesWithPositions == null) return fallbackPosition;
-    if (_datesWithPositions.isEmpty) return fallbackPosition;
-
-    final found = _datesWithPositions.firstWhere(
-          (element) => element.date == date,
-      orElse: () => null,
-    );
-    if (found != null) return found.position - _selectorWidth / 2;
-
-    if (_datesWithPositions.first.date.isAfter(date)) {
-      return -_selectorWidth / 2;
-    } else {
-      return maxPosition - _selectorWidth / 2;
-    }
   }
 }
--- lib/DatePickerSelector.dart
+++ lib/DatePickerSelector.dart
@@ -1,35 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' hide TextDirection;
+
+import 'DatePickerLine.dart';
 
 class DatePickerSelector extends StatefulWidget {
-  const DatePickerSelector({
+  DatePickerSelector(
+      List<DateWithPosition> datesWithPositions,{
     double width,
     double height,
+    double lineWidth,
     double circleIncreaseCoefficient = 2.0,
     double strokeWidth = 4.0,
-    GestureDragUpdateCallback onPanUpdate,
+    DateTime initDate,
+    OnDateSelectedCallback callback,
   })  : _width = width,
         _height = height,
+        _lineWidth = lineWidth,
         _circleRadius = width / 2,
         _circleIncreasedRadius = width / 2 * circleIncreaseCoefficient,
         _strokeWidth = strokeWidth,
-        _onPanUpdate = onPanUpdate;
+        _initDate = initDate,
+        _datesWithPositions = datesWithPositions,
+        _callback = callback;
 
   final double _width;
   final double _height;
+  final double _lineWidth;
   final double _circleRadius;
   final double _circleIncreasedRadius;
   final double _strokeWidth;
-  final GestureDragUpdateCallback _onPanUpdate;
+  final DateTime _initDate;
+  final List<DateWithPosition> _datesWithPositions;
+  final OnDateSelectedCallback _callback;
 
   @override
-  _DatePickerSelectorState createState() => _DatePickerSelectorState(
-        _width,
-        _height,
-        _circleRadius,
-        _circleIncreasedRadius,
-        _strokeWidth,
-        _onPanUpdate,
-      );
+  _DatePickerSelectorState createState() {
+    return _DatePickerSelectorState(
+      _width,
+      _height,
+      _lineWidth,
+      _circleRadius,
+      _circleIncreasedRadius,
+      _strokeWidth,
+      _initDate,
+      _datesWithPositions,
+      _callback,
+    );
+  }
 }
 
 class _DatePickerSelectorState extends State<DatePickerSelector>
@@ -37,69 +54,122 @@ class _DatePickerSelectorState extends State<DatePickerSelector>
   _DatePickerSelectorState(
     double width,
     double height,
+    double lineWidth,
     double circleRadius,
     double circleIncreasedRadius,
     double strokeWidth,
-    GestureDragUpdateCallback onPanUpdate,
+    DateTime initDate,
+    List<DateWithPosition> datesWithPositions,
+    OnDateSelectedCallback callback,
   )   : _width = width,
         _height = height,
+        _lineWidth = lineWidth,
         _circleRadius = circleRadius,
         _circleIncreasedRadius = circleIncreasedRadius,
         _strokeWidth = strokeWidth,
-        _onPanUpdate = onPanUpdate;
+        _date = initDate,
+        _datesWithPositions = datesWithPositions,
+        _callback = callback {
+    _selectorPosition = _positionByDate(initDate);
+  }
 
   final double _width;
   final double _height;
+  final double _lineWidth;
   final double _circleRadius;
   final double _circleIncreasedRadius;
   final double _strokeWidth;
-  final GestureDragUpdateCallback _onPanUpdate;
+  List<DateWithPosition> _datesWithPositions;
+  final OnDateSelectedCallback _callback;
+
+  DateTime _date;
 
   Animation<double> _increaseAnimation;
   AnimationController _increaseAnimationController;
 
   double _circleIncreaseCoefficient = 0.0;
+  double _selectorPosition;
 
   @override
   void initState() {
     super.initState();
     _increaseAnimationController = AnimationController(
-        duration: const Duration(milliseconds: 700),
-        reverseDuration: const Duration(milliseconds: 200),
-        vsync: this);
+      duration: const Duration(milliseconds: 700),
+      reverseDuration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+  }
+
+  @override
+  void didUpdateWidget(DatePickerSelector oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (datesDidChanged(oldWidget)) {
+      setState(() {
+        _datesWithPositions = oldWidget._datesWithPositions;
+        _selectorPosition = _positionByDate(_date);
+      });
+    }
+  }
+
+  bool datesDidChanged(DatePickerSelector oldWidget) {
+    if (_datesWithPositions == null || _datesWithPositions.isEmpty) {
+      return oldWidget._datesWithPositions != null &&
+          oldWidget._datesWithPositions.isNotEmpty;
+    }
+    if (oldWidget._datesWithPositions == null ||
+        oldWidget._datesWithPositions.isEmpty) {
+      return _datesWithPositions != null && _datesWithPositions.isNotEmpty;
+    }
+
+    final first = _datesWithPositions.first;
+    final oldFirst = oldWidget._datesWithPositions.first;
+    return first.date != oldFirst.date || first.position != oldFirst.position;
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanStart: (details) {
-        _increaseAnimationController.forward();
-        _increaseAnimation = Tween(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
-          CurvedAnimation(
-            parent: _increaseAnimationController,
-            curve: Curves.bounceOut,
-            reverseCurve: Curves.linear,
-          ),
-        )..addListener(() {
-            setState(() {
-              _circleIncreaseCoefficient = _increaseAnimation.value;
+    return AnimatedPositioned(
+      height: _height,
+      duration: Duration(milliseconds: 30),
+      left: _selectorPosition,
+      child: GestureDetector(
+        onPanStart: (details) {
+          _increaseAnimationController.forward();
+          _increaseAnimation = Tween(
+            begin: 0.0,
+            end: 1.0,
+          ).animate(
+            CurvedAnimation(
+              parent: _increaseAnimationController,
+              curve: Curves.bounceOut,
+              reverseCurve: Curves.linear,
+            ),
+          )..addListener(() {
+              setState(() {
+                _circleIncreaseCoefficient = _increaseAnimation.value;
+              });
             });
+        },
+        onPanEnd: (details) {
+          _increaseAnimationController.animateBack(0.0);
+        },
+        onPanUpdate: (details) {
+          setState(() {
+            final pointer = details.globalPosition.dx - (_width / 2);
+            _date = _findCloserDateByPosition(pointer);
+            _callback.call(_date);
+            _selectorPosition = _positionByDate(_date);
           });
-      },
-      onPanEnd: (details) {
-        _increaseAnimationController.animateBack(0.0);
-      },
-      onPanUpdate: _onPanUpdate,
-      child: CustomPaint(
-        size: Size(_width, _height),
-        painter: _DatePickerSelectorPainter(
-          _circleRadius,
-          _circleIncreasedRadius,
-          _strokeWidth,
-          _circleIncreaseCoefficient,
+        },
+        child: CustomPaint(
+          size: Size(_width, _height),
+          painter: _DatePickerSelectorPainter(
+            _circleRadius,
+            _circleIncreasedRadius,
+            _strokeWidth,
+            _circleIncreaseCoefficient,
+            _date,
+          ),
         ),
       ),
     );
@@ -110,7 +180,35 @@ class _DatePickerSelectorState extends State<DatePickerSelector>
     _increaseAnimationController.dispose();
     super.dispose();
   }
+
+  DateTime _findCloserDateByPosition(double position) {
+    if (_datesWithPositions != null) {
+      for (DateWithPosition date in _datesWithPositions) {
+        final delta = (date.position - position).abs();
+        if (delta <= 5.0) {
+          return date.date;
+        }
+      }
+    }
+    return null;
+  }
+
+  double _positionByDate(DateTime date) {
+    final found = _datesWithPositions.firstWhere(
+      (element) => element.date == date,
+      orElse: () => null,
+    );
+    if (found != null) return found.position - _width / 2;
+
+    if (_datesWithPositions.first.date.isAfter(date)) {
+      return -(_width / 2);
+    } else {
+      return _lineWidth - _width / 2;
+    }
+  }
 }
+
+typedef OnDateSelectedCallback = Function(DateTime date);
 
 class _DatePickerSelectorPainter extends CustomPainter {
   _DatePickerSelectorPainter(
@@ -118,11 +216,17 @@ class _DatePickerSelectorPainter extends CustomPainter {
     this.circleIncreasedRadius,
     this.strokeWidth,
     this.circleIncreaseCoefficient,
-  );
+    this.date,
+  ) : _paint = Paint()
+          ..color = Colors.lightBlueAccent
+          ..strokeWidth = strokeWidth;
 
   final double circleRadius;
   final double circleIncreasedRadius;
   final double strokeWidth;
+  final DateTime date;
+
+  final Paint _paint;
 
   // from 0.0 to 1.0
   final double circleIncreaseCoefficient;
@@ -130,13 +234,10 @@ class _DatePickerSelectorPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final centerX = size.width / 2;
-    final paint = Paint()
-      ..color = Colors.lightBlueAccent
-      ..strokeWidth = strokeWidth;
     canvas.drawLine(
       Offset(centerX, 0),
       Offset(centerX, size.height),
-      paint,
+      _paint,
     );
 
     // half of increase cause it is circle coefficient (diameter), not radius coefficient
@@ -148,7 +249,29 @@ class _DatePickerSelectorPainter extends CustomPainter {
     canvas.drawCircle(
       Offset(centerX, circleCenterY),
       circleCurrentRadius,
-      paint,
+      _paint,
+    );
+
+    _drawDate(canvas, centerX, circleCenterY);
+  }
+
+  void _drawDate(Canvas canvas, double x, double y) {
+    final fontSize = 20.0;
+    final dateString = "${DateFormat.d().format(date)}";
+    TextPainter textPainter = TextPainter(
+      text: TextSpan(
+        text: dateString,
+        style: TextStyle(color: Colors.white, fontSize: fontSize),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout();
+    textPainter.paint(
+      canvas,
+      Offset(
+        x - (dateString.length * fontSize / 4) - strokeWidth / 4,
+        y - fontSize / 2 - strokeWidth / 2,
+      ),
     );
   }
 
